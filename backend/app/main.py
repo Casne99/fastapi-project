6164c342166b3aed0035a6d249a643ba43613d75
luca.casnedi <luca.casnedi@studenti.unimi.it>
2026-03-06 21:54:35 +0100

    REFACTORING: removed warning

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
 from app.models import GeneratedCode
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from jose import jwt
 from app import config
 
@@ -26,7 +26,7 @@ def get_codes_count(db: Session = Depends(get_db)):
 
 @app.get("/token")
 def get_token():
-    expire = datetime.utcnow() + timedelta(minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES)
     payload = {"exp": expire, "sub": "user_id"}
     token = jwt.encode(payload, config.SECRET_KEY, algorithm=config.ALGORITHM)
     return {"token": token}
