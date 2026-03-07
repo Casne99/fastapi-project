00b42eed23c701690f6b48654be9debc13d0ab77
luca.casnedi <luca.casnedi@studenti.unimi.it>
2026-03-07 09:19:53 +0100

    fix: adjusted build and volumes directories

--- src/backend/app/models.py
+++ src/backend/app/models.py
@@ -1,1 +1,9 @@
+from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
+
+class Credentials(Base):
+    __tablename__ = "credentials"
+    user = Column(String(255), nullable=False, primary_key=True)
+    password = Column(String(255), nullable=False)  # hash bcrypt
